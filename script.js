637358ea933c3e77da1bfb0901f1d47fa673e01a
NeonKaiser <128339548+NeonArceuthobium@users.noreply.github.com>
2025-02-04 19:03:50 +0000

    Add external library and enhance styling and functionality

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("website is working");
+// get
+console.log(document.querySelector("body"));
+
+//set
+let myBody = document.querySelector("body");
+
+//properties
+myBody.style.backgroundColor = colorkit.random();
+colorkit.sparkle(myBody);
